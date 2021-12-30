38c17c8eeea9b828da7ceabaa7c70f7c9f918ebb
JP <91945706+Jankiparmar311@users.noreply.github.com>
2021-12-30 23:03:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,44 +75,5 @@ app.delete("/api/deleteuser/:username",async (req,res) =>{
 });
 
 
-//
-/*app.post("/registration", (req, res) => {
-  const uname = req.body.username;
-  const pass = req.body.password;
-  const name = req.body.name;
-  const age = req.body.age;
-  const user = user.filter((u) => u.username === uname);
-
-  if (user.length === 0) {
-    return res.json({ data: "registration successfull" });
-  } else {
-    return res.json({ data: "username already taken. Please choose another" });
-  }
-});*/
-
 app.listen(port, () => console.log(`server running on port 5000`));
 
-/*app.post("/login", (req, res) => {
-  const uname = req.body.username;
-  const pass = req.body.password;
-  const user = user.filter((u) => u.username === uname && u.password === pass);
-
-  if (user.length === 0) {
-    return res.json({ data: "wrong credentials! please try again." });
-  } else {
-    return res.json({ data: "login successfull" });
-  }
-});
-// http://localhost:5000/list/?name=josh&age=25
-app.get("/list", (req, res) => {
-  const name = req.query.name;
-  const age = req.query.age;
-
-  let userList = user.filter((u) => u.name === name && u.age > parseInt(age));
-
-  if (userList.length === 0) {
-    return res.json({ data: "user not found" });
-  } else {
-    return res.json({ data: userList });
-  }
-});*/
